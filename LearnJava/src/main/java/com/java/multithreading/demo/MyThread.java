c3c748e896cfa9e0c1dac3d2c5e74ca2f56e072d
Prateek Lal <jojohlal@gmail.com>
2026-03-22 18:05:16 +0530

    fix: resolve all LearnJava compilation errors for Java 21

--- LearnJava/src/main/java/com/java/multithreading/demo/MyThread.java
+++ LearnJava/src/main/java/com/java/multithreading/demo/MyThread.java
@@ -1,12 +1,5 @@
 package com.java.multithreading.demo;
 
-import com.java.multithreading.demo.ThreadDemo1.ThreadDem2;
+public class MyThread extends Thread {
 
-public class MyThread extends Thread{
-
-	int a = new ThreadDem2().a;
-
-	
-
-	}// run ends
-// class ends
+}
